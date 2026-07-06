f30461c8f830d3da2dd74008506e3191961c4b1c
Jaron Arbet <jarbet@coh.org>
2026-07-06 11:36:00 -0700

    feat: add `stats_col_label` parameter to `table_overall` with input validation

--- R/table_overall.R
+++ R/table_overall.R
@@ -5,13 +5,20 @@
 #'
 #' @param d data.frame Dataset containing variables to summarize.
 #' @param missing character(1) How to display missing data. Options are "ifany", "no", or "always". Default is "ifany".
+#' @param stats_col_label character(1) Label for the summary column in the table. Default is "Summary".
 #' @return A gtsummary::tbl_summary object for the overall cohort.
 #' @examples
 #' df <- data.frame(age = rnorm(20, 50, 10), sex = sample(c("M","F"), 20, TRUE))
 #' table_overall(df)
 #' @export
-table_overall <- function(d, missing = c("ifany", "no", "always")) {
+table_overall <- function(
+    d,
+    missing = c("ifany", "no", "always"),
+    stats_col_label = 'Summary'
+) {
     missing <- match.arg(missing)
+    stopifnot(is.data.frame(d))
+    stopifnot(is.character(stats_col_label) && length(stats_col_label) == 1)
     gtsummary::theme_gtsummary_language("en", big.mark = ",")
     gtsummary::theme_gtsummary_compact()
     tab_overall <- gtsummary::tbl_summary(
@@ -35,7 +42,10 @@ table_overall <- function(d, missing = c("ifany", "no", "always")) {
         )
     ) |>
         gtsummary::modify_header(
-            gtsummary::all_stat_cols() ~ '**{level}**<br>N = {format(N, big.mark = ",", scientific = FALSE)}'
+            gtsummary::all_stat_cols() ~ sprintf(
+                '**%s**<br>N = {format(N, big.mark = ",", scientific = FALSE)}',
+                stats_col_label
+            )
         ) |>
         gtsummary::modify_footnote(gtsummary::everything() ~ NA) |>
         gtsummary::bold_labels()
